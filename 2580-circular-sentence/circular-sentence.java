cb26bf80906943e93d5abf0ff16492a4717cadc9
DEEPIKAS22CSR040 <127317696+DEEPIKAS22CSR040@users.noreply.github.com>
2024-11-02 22:38:20 +0530

    Time: 1 ms (96.81%) | Memory: 42.1 MB (25.89%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String s[]=sentence.split(" ");
+        int n=s.length;
+        if(s[0].charAt(0)!=s[n-1].charAt(s[n-1].length()-1))
+        return false;
+        for(int i=0;i<s.length-1;i++)
+        {
+            if(s[i].charAt(s[i].length()-1)!=s[i+1].charAt(0))
+            return false;
+        }
+        return true;
+        
+    }
+}
