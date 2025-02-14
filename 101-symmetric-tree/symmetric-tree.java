fa2a88d5fd2bef91e77a56ed637c02aba42aad0e
Deepika S <deepikas.22cse@kongu.edu>
2025-02-14 14:29:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (91.21%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,16 +15,16 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return mirror(root.left,root.right);
+        return mirror(root.left,root.right);        
         
     }
-    boolean mirror(TreeNode n1,TreeNode n2)
+    public boolean mirror(TreeNode n1,TreeNode n2)
     {
         if(n1==null && n2==null)
         return true;
         if(n1==null || n2==null)
         return false;
-        return n1.val==n2.val && mirror(n1.left,n2.right) && mirror(n1.right,n2.left);
-
+        return n1.val==n2.val&& mirror(n1.left,n2.right)&&mirror(n1.right,n2.left);
     }
+   
 }
